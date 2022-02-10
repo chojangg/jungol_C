f58b74d9f90e3ce546fdcba1eab2be3039d5d790
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-02-10 11:15:16 +0900

    문제3

---
+++ 입력/입력 문제3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+
+	a = 10;
+	b = 20;
+	printf("%d + %d = %d\n", a, b, a + b);
+
+	a = 30;
+	b = 40;
+	printf("%d + %d = %d\n", a, b, a + b);
+
+	return 0;
+}
