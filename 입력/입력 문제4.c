0b89e6c74e253eaf980456f578258c484b3d6911
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-02-10 11:20:40 +0900

    문제4

---
+++ 입력/입력 문제4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int r = 5;
+	double pi = 3.14;
+
+	printf("¿øÁÖ = %d * 2 * %f = %f \n", r, pi, r * 2 * pi);
+	printf("³ĞÀÌ = %d * %d * %f = %f\n", r, r, pi, r * r * pi);
+
+	return 0;
+}
