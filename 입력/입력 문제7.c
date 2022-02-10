1f83928a527faaf8476e6959129ba31826e4a419
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-02-10 11:42:09 +0900

    문제7

---
+++ 입력/입력 문제7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+
+	printf("두 수를 입력하시오. ");
+	scanf("%d %d", &a, &b);
+	printf("%d + %d = %d\n", a, b, a + b);
+	printf("%d * %d = %d\n", a, b, a * b);
+
+	return 0;
+}
