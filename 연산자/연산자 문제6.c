814e6f156ea1596ad6769988499eee59a1abb097
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-02-13 14:50:48 +0900

    문제6

---
+++ 연산자/연산자 문제6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	int result1, result2, result3, result4;
+	scanf("%d %d %d", &a, &b, &c);
+
+	result1 = (a > b);
+	result2 = (b >= c);
+	result3 = (a <= b);
+	result4 = (b < c);
+
+	return 0;
+}
