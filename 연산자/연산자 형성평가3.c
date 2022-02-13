ca711874fb1a1a61f1c80c10fa85b6e763763185
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-02-13 15:22:42 +0900

    형성평가4

---
+++ 연산자/연산자 형성평가3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int width, length;
+	scanf("%d %d", &width, &length);
+
+	width += 5;
+	length *= 2;
+	printf("width = %d\n", width);
+	printf("length = %d\n", length);
+	printf("area = %d\n", width * length);
+
+	return 0;
+}
