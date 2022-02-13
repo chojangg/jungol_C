4da9628cf840e930df2ba15d20bf5fce65bfac26
ⒶⓁⒾⒸⒾⒶ <83991079+chojangg@users.noreply.github.com>
2022-02-13 14:50:37 +0900

    문제5

---
+++ 연산자/연산자 문제5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	int result1, result2, result3, result4;
+	scanf("%d %d %d", &a, &b, &c);
+
+	result1 = (a == b);
+	result2 = (b == c);
+	result3 = (a != b);
+	result4 = (b != c);
+	printf("%d %d %d %d \n", result1, result2, result3, result4);
+
+	return 0;
+}
